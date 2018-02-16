29f5c5db50226bef6d8dd62769d7977d15fe6625
Francisco Sánchez Oliver <spuzi@hotmail.es>
2018-02-16 12:39:09 +0100

    basic bubbles

---
+++ Bubble.js
@@ -0,0 +1,28 @@
+﻿/**
+ * @param {*} pos : Initial cell's position
+ * @param {*} rad : Initial cell's radius
+ */
+function Bubble(pos , rad) {
+    this.position = pos;
+    this.radius = rad;
+
+    /**
+     * Move the bubble through screen
+     */
+    this.move = function () {
+        let velocity = p5.Vector.random2D();
+        this.position.add(velocity);
+        this.show();
+    }
+
+    /**
+     * Draws the cell into the screen
+     */
+    this.show = function () {
+        //fill(color(random(50, 255), random(50, 255), random(50, 255)));
+        stroke(255);
+        noFill();
+        ellipse(this.position.x, this.position.y, this.radius, this.radius);
+    }
+
+}
